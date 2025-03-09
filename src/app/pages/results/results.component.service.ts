2760d0541f7318cec2bf1e9a56fc280e0b613dbe
HashanEranga <ccmrhe@gmail.com>
2025-03-09 15:03:53 +0530

    feat(): Added external services

--- src/app/pages/results/results.component.service.ts
+++ src/app/pages/results/results.component.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResultsComponentService {
-  private baseUrl = 'http://127.0.0.1:8000'; 
+  private baseUrl = environment.API_URL; 
 
   constructor(private http: HttpClient) {}
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    API_URL: 'http://13.60.95.188:8000/',
+};
+  
